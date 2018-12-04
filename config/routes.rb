6d32544fc367295f19258f0f6ea3533927a3813c
Juliette Audema <juliette.audema@gmail.com>
2018-12-04 11:31:11 +0100

    Config devise.rb + routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :managers, path: 'managers', controllers: {
+    sessions: 'managers/sessions'
+  }
+  devise_for :users, path:  'users', controllers: {
+    sessions: 'users/sessions'
+  }
+
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
