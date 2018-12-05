097910de910cb1ba76566b0f90b3f9c173136be8
Crysicia <lucienmontmartre@yahoo.fr>
2018-12-05 13:32:38 +0100

    Add map controller/view and GMap Api key

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   }
 
   root 'home#index'
+  resources :map, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
