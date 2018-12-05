7499ec28ec0d007c2400552991430cd0f74ef4a6
Juliette Audema <juliette.audema@gmail.com>
2018-12-05 19:03:25 +0100

    Add path to header

--- app/controllers/concerns/accessible.rb
+++ app/controllers/concerns/accessible.rb
@@ -13,7 +13,7 @@ module Accessible
     elsif current_user
       flash.clear
       # The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
-      redirect_to(authenticated_user_root_path) && return
+      redirect_to(root_path) && return
     end
   end
 end
