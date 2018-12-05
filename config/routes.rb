cfe280f4d76cf89c8f520a4bf62b93b8b9905b1f
louismn <louis.martindunord@eemi.com>
2018-12-05 13:35:05 +0100

    add of the shared

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
 
+  get 'result', to: 'resultpage#index'
+
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
