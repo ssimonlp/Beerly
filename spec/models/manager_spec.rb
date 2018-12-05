7f434f3cb79b5629f5439d6c304873f0d48d4edc
Juliette Audema <juliette.audema@gmail.com>
2018-12-05 18:40:53 +0100

    Create rspec for manager

--- spec/models/manager_spec.rb
+++ spec/models/manager_spec.rb
@@ -7,6 +7,15 @@ RSpec.describe Manager, type: :model do
       expect(manager).to eq(false)
     end 
 
+    it 'ensures format of the e-mail' do 
+      manager = Manager.new(first_name:'Lucien', last_name:'Passera', email:'etudiant.thpgmail.com',  password:'1234567', password_confirmation:'1234567').save 
+      manager2 = Manager.new(first_name:'Lucien', last_name:'Passera', email:'etudiant.thp@gmailcom',  password:'1234567', password_confirmation:'1234567').save 
+      manager3 = Manager.new(first_name:'Lucien', last_name:'Passera', email:'etudiant.thp@gmailcom',  password:'1234567', password_confirmation:'1234567').save 
+      expect(manager).to eq(false)
+      expect(manager2).to eq(false)
+      expect(manager3).to eq(false)
+    end 
+
     it 'ensures first_name presence' do
       manager = Manager.new(last_name:'Passera', email:'etudiant.thp@gmail.com', password:'1234567', password_confirmation:'1234567').save 
       expect(manager).to eq(false)
