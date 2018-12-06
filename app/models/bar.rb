bd4f48a0c9e97a0a19b5e5655694bcebcf5eb0d2
Juliette Audema <juliette.audema@gmail.com>
2018-12-06 17:43:37 +0100

    Correct the model bar and relationships with beers

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,8 +1,9 @@
 class Bar < ApplicationRecord
   include PgSearch
   belongs_to :manager
-  has_many :beers, through: :beer_lists
   has_one :beer_list
+  has_many :beers, through: :beer_list
+  
   
   # Geocoding
   geocoded_by :address
