2c4273e82716e349c49139f4cdfece0129f9641f
Juliette Audema <juliette.audema@gmail.com>
2018-12-06 12:12:19 +0100

    Optimize routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,18 @@ Rails.application.routes.draw do
   
   get 'mentionlegale/index'
   get 'apropos/index'
-  resources :bars
+  resources :bars, only: [:show]
+
+
+
+  devise_for :managers, path: 'managers', controllers: {
+    sessions: 'managers/sessions', registrations: 'managers/registrations'
+  }
 
   resource :managers do 
     resources :bars, only: [:edit, :destroy, :create, :update, :new]
   end 
 
-  devise_for :managers, path: 'managers', controllers: {
-    sessions: 'managers/sessions'
-  }
   devise_for :users, path:  'users', controllers: {
     sessions: 'users/sessions'
   }
