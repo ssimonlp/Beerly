aee17bed8843da3ee631989488de95416b359ed8
Crysicia <lucienmontmartre@yahoo.fr>
2018-12-10 15:17:08 +0100

    Removed useless method call

--- app/services/beers_feeder.rb
+++ app/services/beers_feeder.rb
@@ -33,7 +33,7 @@ class BeersFeeder
     ids.each do |beer_id|
       beer = HTTParty.get("https://api.brewerydb.com/v2/beer/#{beer_id}?withBreweries=Y&key=" + @key)
       parsed_data = beer.parsed_response['data']
-      
+      p beer
       data << [parsed_data['name'], parsed_data['labels'] ? parsed_data['labels']['icon'] : nil, parsed_data['description'], parsed_data['abv'], parsed_data['style']['categoryId'], parsed_data['style']['id'], parsed_data['breweries'][0]['id']]
     end
     data
@@ -41,7 +41,7 @@ class BeersFeeder
   
   def write(data)
    header = ["name", "image_url", "description", "abv", "category_id", "style_id", "brewery_id"]
-    CSV.open("db/beers.csv", "wb") do |file|
+    CSV.open("db/beers.csv", "wb", quote_char: '"') do |file|
       file << header
       data.each do |beer|
         file << beer
@@ -50,7 +50,6 @@ class BeersFeeder
   end
 
   def perform
-    initialize
     beers = get_beers
     ids = get_ids(beers)
     data = get_data(ids)
