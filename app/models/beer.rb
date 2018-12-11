a953a5b1e34e7a4503aadd460ffc200c180d5bfd
ssimonlp <simon.lepavec@gmail.com>
2018-12-11 17:05:56 +0100

    first version of autocomplete search

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,8 +1,16 @@
 class Beer < ApplicationRecord
+  include PgSearch
   belongs_to :brewery
   belongs_to :category
   belongs_to :style
   has_many :bars, through: :beer_lists
+  pg_search_scope :search_beer, 
+    against: :name, 
+    using: {
+      tsearch: {
+        prefix: true
+      }
+    }
 
   def self.search(term)
     if term
