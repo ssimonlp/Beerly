6585346c8030662b5cfbeb8c6f485383d193087a
ssimonlp <simon.lepavec@gmail.com>
2018-12-13 20:56:28 +0100

    implements search by beer category

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,21 @@
-require 'pry'
 class ApplicationController < ActionController::Base
   add_flash_types :success, :warning, :danger, :info
 
   layout :layout_by_resource
   
   def location
-  binding.pry
     respond_to do |format|
-      if params["beer"]
+      if params[:type] == "a"
         format.json {
           search = Bar.search(params["term"], [params["lat"], params["lng"]]);
           render json: { :data => search }
         }
-      elsif params["category"]
+      elsif params[:type] == "b"
         format.json {
           search = Bar.search_cat(params["term"], [params["lat"], params["lng"]]);
           render json: { :data => search }
         }
-     end
+        end
     end
   end
   
