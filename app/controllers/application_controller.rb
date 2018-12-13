26e5f579c452550124b1c1bad76d36e888acbe66
ssimonlp <simon.lepavec@gmail.com>
2018-12-13 17:50:00 +0100

    links back and front search category

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,23 @@
+require 'pry'
 class ApplicationController < ActionController::Base
   add_flash_types :success, :warning, :danger, :info
 
   layout :layout_by_resource
   
   def location
+  binding.pry
     respond_to do |format|
-      format.json {
-        search = Bar.search(params["beer"], [params["lat"], params["lng"]]);
-        render json: { :data => search }
+      if params["beer"]
+        format.json {
+          search = Bar.search(params["term"], [params["lat"], params["lng"]]);
+          render json: { :data => search }
         }
+      elsif params["category"]
+        format.json {
+          search = Bar.search_cat(params["term"], [params["lat"], params["lng"]]);
+          render json: { :data => search }
+        }
+     end
     end
   end
   
