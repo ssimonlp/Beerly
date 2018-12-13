fb739d742e2d8c53a26a6283e34b1ded15d1f6e0
mathieumllt <mathieu.maillet@ymail.com>
2018-12-13 13:57:00 +0100

    decomment mailjet

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -1,5 +1,5 @@
-# Mailjet.configure do |config|
-#   config.api_key = Rails.application.credentials.mailjet[:public_key]  
-#   config.secret_key = Rails.application.credentials.mailjet[:secret_key]  
-#   config.default_from = 'thpstudent@gmail.com'
-# end
+Mailjet.configure do |config|
+  config.api_key = Rails.application.credentials.mailjet[:public_key]  
+  config.secret_key = Rails.application.credentials.mailjet[:secret_key]  
+  config.default_from = 'thpstudent@gmail.com'
+end
