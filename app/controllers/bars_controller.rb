f959933c1279c3b9ab30b53ea02b577c243bed8a
Crysicia <lucienmontmartre@yahoo.fr>
2018-12-16 14:16:50 +0100

    Fix bars controller

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,10 +1,9 @@
 class BarsController < ApplicationController
-
   before_action :manager_has_one_bar, only: [:new]
   before_action :manager_can_only_edit_their_bar, only: [:edit]
 
   def index
-    @bars = Bar.all 
+    @bars = Bar.all
   end
 
   def show
@@ -15,27 +14,26 @@ class BarsController < ApplicationController
   end
 
   def new
-    @bar = Bar.new 
+    @bar = Bar.new
   end
 
   def update
     if address_or_name_blank
-      redirect_to edit_managers_bar_path(current_manager.bar.id), alert: "Le nom ou l'adresse du bar ne peuvent être vides."
-    else  
+      redirect_to edit_managers_bar_path(current_manager.bar.id), alert: 'Le nom ou l\'adresse du bar ne peuvent être vides.'
+    else
       @bar = Bar.find(params[:id])
       @bar.update(bar_params)
       redirect_to bar_path(@bar.id)
-    end 
+    end
   end
 
   def edit
     @bar = Bar.find(current_manager.bar.id)
   end
 
+  private
 
-  private 
-    def bar_params
-      params.require(:bar).permit(:name, :address, :photo, :opening_time, :happy_hours, :description)
-    end 
-
+  def bar_params
+    params.require(:bar).permit(:name, :address, :photo, :opening_time, :happy_hours, :description)
+  end
 end
