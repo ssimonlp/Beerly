9286c8f61622c33480e211a07b7954fada736287
Juliette Audema <juliette.audema@gmail.com>
2018-12-16 15:13:42 +0100

    Fix the things that broke everything

--- app/controllers/fav_beer_controller.rb
+++ app/controllers/fav_beer_controller.rb
@@ -27,4 +27,11 @@ class FavBeerController < ApplicationController
       params.require(:fav_beer).permit(:beer_id)
     end 
 
+    def has_already_three_favorite_beers
+      current_user.beer_wishlist.fav_beers.count == 3
+    end
+  
+    def beer_is_already_in_favorites
+      wishlist.beers.include?(Beer.find((params[:fav_beer][:beer_id]).to_i))
+    end
 end
--- app/helpers/bars_helper.rb
+++ app/helpers/bars_helper.rb
@@ -1,19 +1,2 @@
 module BarsHelper
-
-  def manager_has_one_bar 
-    if current_manager.bar
-      redirect_to root_path
-    end 
-  end 
-
-  def manager_can_only_edit_their_bar 
-    if current_manager.bar.id != params[:id].to_i
-      redirect_to edit_managers_bar_path(current_manager.bar.id), alert: "Vous n'avez pas accès à cette page"
-    end 
-  end 
-
-  def address_or_name_blank
-    params[:bar][:address].blank? || params[:bar][:name].blank?
-  end 
-
 end
