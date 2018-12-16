22f9d44eeaac01de1a48937a4cc5e539daf5240e
Crysicia <lucienmontmartre@yahoo.fr>
2018-12-16 14:51:55 +0100

    Fix beer wishlist controller

--- app/controllers/beer_wishlist_controller.rb
+++ app/controllers/beer_wishlist_controller.rb
@@ -1,18 +1,13 @@
 class BeerWishlistController < ApplicationController
   before_action :check_wishlist
-  
-  def index 
-    @BeerWishlist = BeerWishlist.new
-  end 
-  
-  
+
   def show
     @beer_wishlist = BeerWishlist.find_by(user_id: current_user.id)
   end
+
   private
-    def check_wishlist
-      if BeerWishlist.find_by(user_id: current_user.id) == nil
-        BeerWishlist.create(user_id: current_user.id)
-      end
-    end
+
+  def check_wishlist
+    BeerWishlist.create(user_id: current_user.id) if BeerWishlist.find_by(user_id: current_user.id).nil?
+  end
 end
