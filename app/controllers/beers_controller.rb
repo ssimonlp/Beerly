ca7626e72558733f838d9af272152fdcd8bf8bed
Crysicia <lucienmontmartre@yahoo.fr>
2018-12-16 15:02:57 +0100

    Fix beers controller

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,20 +1,15 @@
 class BeersController < ApplicationController
-  
-
   def new
     @beer = Beer.new
   end
 
   def index
     @beers = Beer.search(params[:term])
-    @beerlist = BeerList.new 
-    @fav_beer = FavBeer.new 
+    @beerlist = BeerList.new
+    @fav_beer = FavBeer.new
   end
 
   def show
     @beer = Beer.find(params[:id])
   end
-     
 end
-
-
