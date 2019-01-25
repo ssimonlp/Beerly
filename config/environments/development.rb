809ad12b40a1007adb99050056ded5f3e7897fcb
Juliette Audema <juliette.audema@gmail.com>
2019-01-25 18:04:23 +0100

    Fix maildev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,6 +70,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address => "localhost",
-      :port => 1030
+      :port => 1025
   }
 end
