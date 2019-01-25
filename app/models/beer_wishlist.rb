5c24f28a7cca96dbe21a4786ddb9eaf230e8e4aa
Juliette Audema <juliette.audema@gmail.com>
2019-01-25 13:51:01 +0100

    Add husky

--- app/models/beer_wishlist.rb
+++ app/models/beer_wishlist.rb
@@ -4,5 +4,5 @@ class BeerWishlist < ApplicationRecord
   belongs_to :user
   has_many :fav_beers, dependent: :destroy
   has_many :beers, through: :fav_beers
-  validates :fav_beers, length: 2
+  validates :fav_beers, length: { maximum: 2 }
 end
